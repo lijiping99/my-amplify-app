117265435458507e410c29d674891ff0ea6ad709
Jiping Li <jiping.li@openlane.com>
2023-01-11 13:26:55 -0500

    version 0.0.1

--- amplify/backend/function/marathonHandler/src/index.js
+++ amplify/backend/function/marathonHandler/src/index.js
@@ -1,9 +1,44 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
 
+async function createItem(params){
+  try {
+    await docClient.put(params).promise();
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+/*
+* runner
+* marathon
+* gender
+* time
+* year
+* age
+*/
 
 exports.handler = async (event) => {
     console.log(event)
     const customerId = event.pathParameters.marathonId;
+    const body = JSON.parse(event.body);
     const customer = {'customerId': customerId, 'customerName': "Customer " + customerId };
+
+    const params = {
+        TableName : 'marathons',
+        
+        Item: {
+           runner: body.name,
+           marathon: body.marathon,
+           gender: body.gender,
+           time: body.time,
+           date: body.date,
+           age: body.age
+        }
+      }
+
+      await createItem(params);
+
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
--- src/App.js
+++ src/App.js
@@ -7,13 +7,36 @@ const myAPI = "apia80f2aef"
 const path = '/marathons'; 
 
 const App = () => {
-  const [input, setInput] = useState("")
+
+  /*
+  const [name, setName] = useState("")
+  const [marathon, setMarathon] = useState("")
+  const [gender, setGender] = useState("")
+  const [time, setTime] = useState("")
+  const [date, setDate] = useState("")
+  const [age, setAge] = useState("")
+ */
+  const [inputs, setInputs] = useState({});
+ 
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+    getMarathon(inputs);
+  }
+
   const [marathons, setMarathons] = useState([])
 
   //Function to fetch from our backend and update customers array
   function getMarathon(e) {
     let marathonId = e.input
-    API.get(myAPI, path + "/" + marathonId)
+    
+    API.post(myAPI, path + "/" + marathonId,{body: e,headers: {}})
        .then(response => {
          console.log(response)
          let newMarathons = [...marathons]
@@ -29,19 +52,45 @@ const App = () => {
   return (
     
     <div className="App">
-      <h1>Super Simple React App</h1>
+      <h1>Super Simple Marathon Ranking App</h1>
       <div>
-          <input placeholder="marathon id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+        <form onSubmit={handleSubmit}>
+        <div>
+        <label>your name:</label>
+        <input placeholder="your name" type="text" name="name" value={inputs.name || ""} onChange={handleChange}/>    
+        </div>
+        <div>
+        <label>marathon city:</label>
+        <input placeholder="" type="text" name="marathon" value={inputs.marathon || ""} onChange={handleChange}/>           
+        </div>
+        <div>
+        <label>marathon date:</label>
+        <input placeholder="yyyymmdd" type="text" name="date" value={inputs.date || ""} onChange={handleChange}/>     
+        </div>
+        <div>
+        <label>finish time:</label>
+        <input placeholder="hh:mm:ss" type="text" name="time" value={inputs.time || ""} onChange={handleChange}/>     
+        </div>
+        <div>
+        <label>your gender:</label>
+        <input placeholder="M/F" type="text" name="gender" value={inputs.gender || ""} onChange={handleChange}/>     
+        </div>
+        <div>
+        <label>age when run this marathon:</label>
+        <input placeholder="35" type="text" name="age" value={inputs.age || ""} onChange={handleChange}/>     
+        </div>
+        <input type="submit" />
+        </form>
       </div>
       <br/>
-      <button onClick={() => getMarathon({input})}>Get Marathon From Backend</button>
+      
 
       <h2 style={{visibility: marathons.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
       {
        marathons.map((thisMarathon, index) => {
          return (
         <div key={thisMarathon.marathonId}>
-          <span><b>marathonId:</b> {thisMarathon.customerId} - <b>MarathonName</b>: {thisMarathon.customerName}</span>
+          <span><b>MarathonId:</b> {thisMarathon.customerId} - <b>MarathonName</b>: {thisMarathon.customerName}</span>
         </div>)
        })
       }
